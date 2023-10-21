8f43727c309ffb959063b3bd495ec055826e65d3
ArinjayAarush <148636715+ArinjayAarush@users.noreply.github.com>
2023-10-21 19:50:13 +0530

    Add files via upload

---
+++ Pygame/Starting Pygame.py
@@ -0,0 +1,28 @@
+import pygame
+import sys
+
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+clock = pygame.time.Clock()
+icon = pygame.image.load("dozen.png")
+pygame.display.set_caption('Dozen')
+pygame.display.set_icon(icon)
+textfont = pygame.font.Font("font/Pixeltype.ttf",50)
+
+sky_surface = pygame.image.load("graphics/Sky.png")
+ground_surface = pygame.image.load("graphics/ground.png")
+text_surface = textfont.render('Aarush OP!', False ,'Black')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    screen.blit(sky_surface,(0,0))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(0,300))
+    pygame.display.update()
+    clock.tick(60)
+    
